4d144d64cb43c38503298e82784cb369b1f81849
Lucas13 <lopezlucasdev@gmail.com>
2026-02-02 11:03:42 -0300

    primer commit

---
+++ src/main/java/com/lucaslopez/LedgerX/LedgerXApplication.java
@@ -0,0 +1,13 @@
+package com.lucaslopez.LedgerX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgerXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LedgerXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucaslopez/LedgerX/LedgerXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucaslopez.LedgerX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
